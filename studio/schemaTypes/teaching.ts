6ee4224797b556e34256a3a998fe5705d618633b
Elliot Cann <elliotcann@gmail.com>
2026-02-09 15:15:52 +0000

    Add teaching.ts file to SchemaTypes folder

---
+++ studio/schemaTypes/teaching.ts
@@ -0,0 +1,84 @@
+// This defines what fields a teaching post will have
+
+export default {
+    name: 'teaching',
+    title: 'Teaching',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            titel: 'Title',
+            type: 'string',
+            validation: (Rule: any) => Rule.required()
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            tpye: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 96,
+            },
+            validation: (Rule: any) => Rule.required()
+        },
+        {
+            name: 'mainImage',
+            title: 'Main Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'categories',
+            title: 'Categories',
+            type: 'array',
+            of: [
+                    {
+                        type: 'reference',
+                        to: {
+                            type: 'category',
+                        },
+                    },
+                ],
+        },
+        {
+            name: 'publishedAt',
+            title: 'Published Date',
+            type: 'datetime',
+        },
+        {
+            name: 'excerpt',
+            title: 'Short Description',
+            type: 'text',
+            rows: 3,
+        },
+        {
+            name: 'body',
+            title: 'content',
+            type: 'array',
+            of: [
+                {
+                    type: 'block',
+                },
+                {
+                    type: 'image',
+                    options: {hotspot: true},
+                    fields: [
+                        {
+                            name: 'caption',
+                            title: 'Caption',
+                            type: 'string',
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+    preview: {
+        select: {
+            title: 'title',
+            media: 'mainImage',
+        },
+    },
+}
