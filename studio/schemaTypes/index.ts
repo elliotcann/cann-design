717f562e6f3bd40925a5dbc9b16257506576ad7e
Elliot Cann <elliotcann@gmail.com>
2026-02-09 15:52:42 +0000

    Add comments to index.ts file

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,7 +1,7 @@
 // This file imports and exports all schemas
 
-import project from './project'
+import project from './project' // Brings in the project schema we created
 import teaching from './teaching'
 import category from './category'
 
-export const schemaTypes = [project, teaching, category]
+export const schemaTypes = [project, teaching, category] // Bundles our schemas together into one list (array) to be used by Sanity
