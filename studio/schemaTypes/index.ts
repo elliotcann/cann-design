d5d79effc6d945d7e3d2b5658f317907ef5e014e
Elliot Cann <elliotcann@gmail.com>
2026-02-09 15:50:33 +0000

    Update index.ts file in SchemaTypes folder

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+// This file imports and exports all schemas
+
+import project from './project'
+import teaching from './teaching'
+import category from './category'
+
+export const schemaTypes = [project, teaching, category]
