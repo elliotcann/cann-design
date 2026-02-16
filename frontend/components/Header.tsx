48216c113d88e166955aa98f64e51ad5e1da3cbc
Elliot Cann <elliotcann@gmail.com>
2026-02-16 10:38:23 +0000

    Update style and responsiveness of nav for all viewports

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -19,12 +19,12 @@ export default function Header() {
     
         <header className="fixed top-0 left-0 right-0 z-50">
 
-            <nav className="max-w-7xl mx-auto px-6 py-4">
+            <nav className="max-w-7xl mx-auto">
 
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between  px-6 py-4 bg-white-400 backdrop-filter backdrop-blur-sm bg-opacity-20">
 
                     {/* Logo */}
-                    <Link href="/">
+                    <Link href="/" className="flex-shrink-0">
                         <Image 
                             src="/header-logo.svg" 
                             alt="CANN Design logo" 
@@ -36,7 +36,7 @@ export default function Header() {
 
                     {/* Desktop Nav Links - hidden on mobile */}
 
-                    <ul className="hidden md:flex w-full justify-center gap-6 lg:gap-8">
+                    <ul className="hidden lg:flex w-full justify-center lg:gap-8">
                         <li className="bg-black text-white text-sm font-bold py-2 px-4 rounded-full opacity-100  transition-opacity hover:opacity-75 duration-300 ease-in-out">
                             <Link href="/projects">
                                 Projects
@@ -70,21 +70,22 @@ export default function Header() {
                     </ul>
 
                     {/* Mobile Nav Button */}
-                    <button 
-                        className="md:hidden bg-black text-white p-2 rounded-full"
-                        onClick={toggleMenu}
-                        aria-label="Toggle menu"
-                    >   
-                        {/* Show X when open, Menu when close */}
-                        {isMenuOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
-                    </button>
+                    <div className="lg:hidden w-10 flex justify-end flex-shrink-0">
+                        <button 
+                            className="bg-black text-white p-2 rounded-full"
+                            onClick={toggleMenu}
+                            aria-label="Toggle menu"
+                        >   
+                            {/* Show X when open, Menu when close */}
+                            {isMenuOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
+                        </button>
+                    </div>
 
                 </div>
 
                 {/* Mobile Nav Menu - shown/hidden based on state*/}
-                {isMenuOpen && (
-                <div className="md:hidden mt-4">
-                    <ul className="flex flex-col gap-3">
+                <div className={`lg:hidden h-screen w-64 fixed top-20 right-0 bg-white-400 backdrop-filter backdrop-blur-sm bg-opacity-20 transition-transform duration-300 ease-in-out ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+                    <ul className="flex flex-col gap-6 mt-8 w-32 justify-self-center">
                         <li className="bg-black text-white text-sm font-bold py-2 px-4 rounded-full text-center">
                             <Link href="/projects">Projects</Link>
                         </li>
@@ -105,7 +106,6 @@ export default function Header() {
                         </li>
                     </ul>
                 </div>
-                )}
 
             </nav>
 
