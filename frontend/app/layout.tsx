3ce0cf88432b610c4c4bc6350bb8e1bea0ef6de8
Elliot Cann <elliotcann@gmail.com>
2026-02-16 11:28:58 +0000

    Add imports for page.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 import Link from "next/link";
 import Header from "@/components/Header";
 
