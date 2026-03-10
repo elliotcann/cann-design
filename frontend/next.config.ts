cd820c45c7566f797f48d93633ed01734692e0d2
Elliot Cann <elliotcann@gmail.com>
2026-03-10 22:15:06 +0000

    Update next.config.ts to allow images from cdn.sanity.io

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io', // Tells Next.js to trust Sanity's image CDN
+      }
+    ]
+  }
 };
 
 export default nextConfig;
