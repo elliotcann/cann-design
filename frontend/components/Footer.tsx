360b973531d56c09d1b08f7cb19e9419431da707
Elliot Cann <elliotcann@gmail.com>
2026-03-10 16:43:26 +0000

    Made Footer its own component and removed from layout.tsx

---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+export default function Footer() {
+
+    return (
+        
+        <footer>
+
+          <div className="font-bold text-sm bg-black text-white px-6 py-4">
+
+            <div className="flex md:flex-row flex-col align-center justify-between tracking-wide">
+
+              <p className="mb-2 md:mb-0">
+                CANN Design {new Date().getFullYear()} &copy;
+              </p>
+
+              <a href="mailto:hello@cann.design" target="_blank" className="mb-2 md:mb-0 opacity-100  transition-opacity hover:opacity-75 duration-300 ease-in-out">
+                  hello@cann.design
+              </a>
+                  
+              <Link href="/privacy" className="opacity-100  transition-opacity hover:opacity-75 duration-300 ease-in-out">
+                  Privacy Policy
+              </Link>
+
+            </div>
+
+          </div>
+          
+        </footer>
+    
+    )
+
+}
