14aa6bb1da5a49b833dd299eb06ca777b58d1611
Elliot Cann <elliotcann@gmail.com>
2026-03-10 20:44:32 +0000

    Update BackToTop for pages that use SmootherScroller and pages that do not

--- frontend/components/BackToTop.tsx
+++ frontend/components/BackToTop.tsx
@@ -4,12 +4,18 @@ import { useState, useEffect } from "react"; // 'useState' lets us remember a va
 
 import { ChevronUp } from "lucide-react"; // Imports up chevron from icon pack we are using
 
-export default function BackToTop() {
+interface Props {
+    currentIndex?: number;
+    onBackToTop?: () => void;
+}
+
+export default function BackToTop({ currentIndex, onBackToTop }: Props) {
 
     const [isVisible, setIsVisible] = useState(false); // 'isVisible' is our memory, it tracks whether the button should show or hide
     // 'useState(false)' means start hidden (false = not visible). 'setIsVisible' is the function we call to UPDATE that memory
 
     useEffect(() => {  // useEffect runs code outside of rendering. Here we use it to add a scroll listener when the component loads and remove it when the component is removed
+        if (currentIndex !== undefined) return; // Only run this on normal pages when an Index is not passed in. Skips it on pages with SmoothScroller
 
         const handleScroll = () => {
             if (window.scrollY > 400) { // 'window.scrollY' how many pixels the user has scrolled. If the scrolled more than 400px, show the button
@@ -27,19 +33,29 @@ export default function BackToTop() {
 
     }, []); // The empty [] means: Only run this effect once, when the component first loads
 
-    const scrollToTop = () => { // This function runs when the button is clicked
-        window.scrollTo({
-            top: 0, // Scroll to the very top of the page
-            behavior: "smooth", // Scroll smoothly, not instantly
-        });
-    };
 
+    // SMOOTHSCROLLER PAGE - If currentIndex was passed in then were in a SmoothScroller page
+    if(currentIndex !== undefined) {
+        if(currentIndex === 0) return null;
+        return (
+            <button
+                onClick={onBackToTop} // Use the function SmoothScroller gave us
+                className="fixed bottom-8 right-8 z-50 bg-black text-white p-3 rounded-full opacity-100 hover:opacity-75 transition-opacity duration-300 ease-in-out"
+                aria-label="Back to top"
+            >
+                <ChevronUp className="w-6 h-6" />
+            </button>
+        );
+    
+
+    }
+
+    // NORMAL PAGE - No currentIndex passed in so use window.scrollY logic
     if (!isVisible) return null; // If isVisible is false, return null -> render nothing (button is hidden)
 
-    
     return ( // Otherwise, render the button!
         <button
-            onClick={scrollToTop} // Calls 'scrollToTop' when button is clicked
+            onClick={() => window.scrollTo({ top:0, behavior: "smooth"})} // Calls 'scrollToTop' when button is clicked
             className="fixed bottom-8 right-8 z-50 bg-black text-white p-3 rounded-full opacity-100 hover:opacity-75 transition-opacity duration-300 ease-in-out" // Custom Tailwind for button styling
             aria-label="Back to top" // Accessability that tells screen readers what the button does
         >
